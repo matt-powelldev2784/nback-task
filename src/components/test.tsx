01863fe507fff71b862acd007b78fc08fab782e3
Matthew Powell <matt.powell2784@gmail.com>
2024-10-07 19:07:19 +0100

    feat: remove bolier plate files and design

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -4,29 +4,9 @@ import App from './App'
 
 describe('<App />', () => {
   it('should render the App', () => {
-    const { container } = render(<App />)
+    render(<App />)
 
-    expect(
-      screen.getByRole('heading', {
-        name: /Welcome!/i,
-        level: 1
-      })
-    ).toBeInTheDocument()
-
-    expect(
-      screen.getByText(
-        /This is a boilerplate build with Vite, React 18, TypeScript, Vitest, Testing Library, TailwindCSS 3, Eslint and Prettier./i
-      )
-    ).toBeInTheDocument()
-
-    expect(
-      screen.getByRole('link', {
-        name: /start building for free/i
-      })
-    ).toBeInTheDocument()
-
-    expect(screen.getByRole('img')).toBeInTheDocument()
-
-    expect(container.firstChild).toBeInTheDocument()
+    const testText = screen.getByText('test')
+    expect(testText).toBeInTheDocument()
   })
 })
