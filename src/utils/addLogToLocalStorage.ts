7079fed0f936231fcffc060b7be8ad2a3ffe6679
Matthew Powell <matt.powell2784@gmail.com>
2024-10-11 18:21:35 +0100

    feat: add function to store game logs

---
+++ src/utils/addLogToLocalStorage.ts
@@ -0,0 +1,28 @@
+type LogType = 'startGame' | 'name' | 'repeatedLetter' | 'endGame'
+
+interface Log {
+  type: LogType
+  date: string
+  name: string
+  correctAnswer?: boolean
+}
+
+export const addNNameToStorage = (name: string) => {
+  localStorage.setItem('name', name)
+}
+
+export const addLogToLocalStorage = ({ type, correctAnswer }: Log) => {
+  const storedName = localStorage.getItem('name')
+
+  const log: Log = {
+    type,
+    date: new Date().toISOString(),
+    name: storedName || '',
+    correctAnswer
+  }
+
+  const existingLogs = JSON.parse(localStorage.getItem('logs') || '[]')
+  const updatedLogs = [...existingLogs, log]
+
+  localStorage.setItem('logs', JSON.stringify(updatedLogs))
+}
