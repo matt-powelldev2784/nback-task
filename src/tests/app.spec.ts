182556bb663bfdec60cd9c9142d772d4aa70586b
Matthew Powell <matt.powell2784@gmail.com>
2024-10-24 19:39:01 +0100

    test: add game screen tests

--- src/tests/app.spec.ts
+++ src/tests/app.spec.ts
@@ -1,27 +1,42 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('end to end test', () => {
-  test('should navigate to enter name screen when start button is clicked', async ({
-    page
-  }) => {
-    // navigate to home page
-    await page.goto('http://localhost:5173/')
+test('end to end test', async ({ page }) => {
+  // navigate to home page
+  await page.goto('http://localhost:5173/')
 
-    // click start game button
-    const startGameButton = page.getByRole('button', { name: 'Start Game' })
-    startGameButton.click()
+  ///////////////////////////////// Home Screen Tests  //////////////////////////////////
+  // click start game button
+  const startGameButton = page.getByRole('button', { name: 'Start Game' })
+  startGameButton.click()
 
-    // expect to see enter name screen
-    await expect(page.getByText('Enter your name to begin:')).toBeVisible()
+  ///////////////////////////////// Enter Name Screen Tests  //////////////////////////////////
+  // expect to see enter name screen
+  await expect(page.getByText('Enter your name to begin:')).toBeVisible()
 
-    // enter name and click submit butotn
-    await page.fill('input', 'John Snmith')
-    const submitButton = page.getByRole('button', { name: 'Submit' })
-    submitButton.click()
+  // enter name and click submit butotn
+  await page.fill('input', 'John Snmith')
+  const submitButton = page.getByRole('button', { name: 'Submit' })
+  submitButton.click()
 
-    //expect to see game screen
-    await expect(
-      page.getByRole('button', { name: 'Repeated Letter' })
-    ).toBeVisible()
+  ///////////////////////////////// Game Screen Tests  //////////////////////////////////
+  //expect to see game screen
+  const repeatedLetterButton = page.getByRole('button', {
+    name: 'Repeated Letter'
   })
+  await expect(repeatedLetterButton).toBeVisible()
+
+  // check repeaed letter button can be clicked
+  // the game will end because the repeated letter button is clicked on the first
+  // two letters - which means it's the no letter can be repeated yet
+  await page.waitForTimeout(4400)
+  await repeatedLetterButton.click()
+  await page.waitForTimeout(2200)
+  await repeatedLetterButton.click()
+  await page.waitForTimeout(2200)
+
+  // expect to see view results button
+  const viewResultsButton = page.getByRole('button', {
+    name: 'View Results'
+  })
+  await expect(viewResultsButton).toBeVisible()
 })
