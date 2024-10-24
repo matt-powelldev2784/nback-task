3ef40398175eb364978b474c37c004c9273daf1d
Matthew Powell <matt.powell2784@gmail.com>
2024-10-24 19:26:16 +0100

    test: refactor test file for end to end test

--- src/tests/app.spec.ts
+++ src/tests/app.spec.ts
@@ -1,16 +1,17 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('homepage', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173/')
-  })
-
+test.describe('end to end test', () => {
   test('should navigate to enter name screen when start button is clicked', async ({
     page
   }) => {
+    // navigate to home page
+    await page.goto('http://localhost:5173/')
+
+    // click start game button
     const startGameButton = page.getByRole('button', { name: 'Start Game' })
     startGameButton.click()
 
+    // expect to see enter name screen
     await expect(page.getByText('Enter your name to begin:')).toBeVisible()
   })
 })
