a04004f17cdb73fd217b251d34e55f722ad7fde1
Matthew Powell <matt.powell2784@gmail.com>
2024-10-27 13:08:15 +0000

    test: extend playwright test timeout to 60000 ms

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,8 @@ import { defineConfig, devices } from '@playwright/test'
  */
 export default defineConfig({
   testDir: './',
+  // set 60,000 ms test timeout due to game takeing at least 30,000 ms
+  timeout: 15 * 2000 + 30000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
